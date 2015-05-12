64d049a3bc2ab03c970a38b193618a1fb4fcefd8
Max Hewett <mjhewett01@gmail.com>
2015-05-12 16:45:48 -0400

    final version

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 var no1 = document.querySelector('#num_one');
 var no2 = document.querySelector('#num_two');
 var answer = document.querySelector('#answer');
-var addBtn = document.querySelector('#addBtn');
+var button = document.querySelector('#calculateBtn');
 
 
 function add(){
 	var numOne = Number(no1.value);
 	var numTwo = Number(no2.value);
 	var sum = numOne + numTwo;
-	addBtn.innerHTML = sum.toString();
-}
-
-function multiply(){
-	of = numOne * numTwo;
-	answer.innerHTML = toString();
+	answer.innerHTML = sum.toString();
 }
 
 
