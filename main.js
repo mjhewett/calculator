47a3119862c000e05d5573d3bf51075b9cd7e342
Max Hewett <mjhewett01@gmail.com>
2015-09-29 15:33:54 -0400

    Added multiply subtract and divide functionality

--- main.js
+++ main.js
@@ -2,19 +2,67 @@ var no1 = document.querySelector('#num_one');
 var no2 = document.querySelector('#num_two');
 var answer = document.querySelector('#answer');
 var addBtn = document.querySelector('#addBtn');
+var subBtn = document.querySelector('#subBtn');
+var multBtn = document.querySelector('#multBtn');
+var divBtn = document.querySelector('#divBtn');
 
 
 function add(){
 	var numOne = Number(no1.value);
 	var numTwo = Number(no2.value);
 	var sum = numOne + numTwo;
-	addBtn.innerHTML = sum.toString();
+	answer.innerHTML = sum.toString();
+}
+
+
+function subtract(){
+  var numOne = Number(no1.value);
+  var numTwo = Number(no2.value);
+  var subtract = numOne - numTwo;
+  answer.innerHTML = subtract.toString();
 }
 
 function multiply(){
-	of = numOne * numTwo;
-	answer.innerHTML = toString();
+
+  var numOne = Number(no1.value);
+  var numTwo = Number(no2.value);
+  var multiply = numOne * numTwo;
+	answer.innerHTML = multiply.toString();
+}
+
+function divide() {
+ var numOne = Number(no1.value);
+ var numTwo = Number(no2.value);
+ var divide = numOne / numTwo;
+ answer.innerHTML = divide.toString();
 }
 
 
-button.addEventListener("click", add);
+addBtn.addEventListener("click", add);
+subBtn.addEventListener("click", subtract);
+multBtn.addEventListener("click", multiply);
+divBtn.addEventListener("click", divide);
+
+
+
+
+
+
+
+
+// var add = document.getElementById('add');
+// var subtract = document.getElementById('subtract');
+// var divide = document.getElementById('divide');
+// var multiply = document.getElementById('multiply');
+// var clear = document.getElementById('clear');
+// var submit = document.getElementById('submit');
+
+// var one = document.querySelector('#one');
+// var two = document.querySelector('#two');
+// var three = document.querySelector('#three');
+// var four = document.querySelector('#four');
+// var five = document.querySelector('#five');
+// var six = document.querySelector('#six');
+// var seven = document.querySelector('#seven');
+
+
